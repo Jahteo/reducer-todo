f720105559ea4a5fc2b2264bbf0be4268eca0ccf
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-08 19:43:36 -0700

    Basic form structure

---
+++ todo/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Form = () => {
+
+  return (
+    <div>
+      <h1>Bestest Form Ever.</h1>
+      <form>
+        <input>
+        </input>
+        <button type="submit"> Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form;
